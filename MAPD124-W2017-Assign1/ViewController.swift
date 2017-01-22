1af6f922a210c946b8a73441b8fff59e75424ea4
Andy <andy@andys-mac.local>
2017-01-22 17:43:39 -0500

    Initial Commit

---
+++ MAPD124-W2017-Assign1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD124-W2017-Assign1
+//
+//  Created by Andy on 2017-01-22.
+//  Copyright © 2017 Reza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
